f94dbcabfe5d66b5559a7b7468a939627980a039
Simon Johnston <johnstonskj@gmail.com>
2019-08-02 07:48:50 -0700

    currency working

--- src/codes/currency.rs
+++ src/codes/currency.rs
@@ -1,10 +1,18 @@
 use std::collections::HashMap;
 
+use serde::{Deserialize, Serialize};
+
+// ------------------------------------------------------------------------------------------------
+// Public Types
+// ------------------------------------------------------------------------------------------------
+
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Subdivision {
     pub exponent: i8,
     pub name: Option<String>
 }
 
+#[derive(Serialize, Deserialize, Debug)]
 pub struct CurrencyInfo {
     pub alphabetic_code: String,
     pub name: String,
@@ -14,13 +22,17 @@ pub struct CurrencyInfo {
     pub subdivisions: Vec<Subdivision>,
 }
 
+// ------------------------------------------------------------------------------------------------
+// Public Functions
+// ------------------------------------------------------------------------------------------------
+
 lazy_static! {
-    static ref CURRENCIES: HashMap<String, CurrencyInfo> = HashMap::new();
-    static ref NUMERIC_LOOKUP: HashMap<u16, String> = HashMap::new();
+    static ref CURRENCIES: HashMap<String, CurrencyInfo> = currencies_from_json();
+    static ref NUMERIC_LOOKUP: HashMap<u16, String> = load_currency_lookup();
 }
 
 pub fn lookup_by_alpha(alphabetic_code: &String) -> Option<&'static CurrencyInfo> {
-    assert_ne!(alphabetic_code.len(), 3, "currency code must be either 2, or 3, digits long.");
+    assert_eq!(alphabetic_code.len(), 3, "currency code must be 3 characters long");
     CURRENCIES.get(alphabetic_code)
 }
 
@@ -39,10 +51,54 @@ pub fn currency_numeric_codes() -> Vec<u16> {
     NUMERIC_LOOKUP.keys().cloned().collect()
 }
 
-pub fn currencies_for_country_name(name: String) -> Vec<&'static CurrencyInfo> {
-    CURRENCIES
-        .values()
-        .filter(|currency|
-            currency.countries.contains(&name)
-        ).collect()
+//pub fn currencies_for_country_name(name: String) -> Vec<&'static CurrencyInfo> {
+//    CURRENCIES
+//        .values()
+//        .filter(|currency|
+//            currency.countries.contains(&name)
+//        ).collect()
+//}
+
+// ------------------------------------------------------------------------------------------------
+// Generated Data
+// ------------------------------------------------------------------------------------------------
+
+fn currencies_from_json() -> HashMap<String, CurrencyInfo> {
+    info!("currencies_from_json - loading JSON");
+    let raw_data = include_bytes!("data/currencies.json");
+    let currency_map: HashMap<String, CurrencyInfo> = serde_json::from_slice(raw_data).unwrap();
+    info!("currencies_from_json - loaded {} currencies", currency_map.len());
+    currency_map
+}
+
+fn load_currency_lookup() -> HashMap<u16, String> {
+    info!("load_currency_lookup - create from CURRENCIES");
+    let mut lookup_map: HashMap<u16, String> = HashMap::new();
+    for currency in CURRENCIES.values() {
+        if let Some(numeric) = &currency.numeric_code {
+            lookup_map.insert(*numeric, currency.alphabetic_code.to_string());
+        }
+    }
+    info!("load_currency_lookup - mapped {} countries", lookup_map.len());
+    lookup_map
+}
+
+// ------------------------------------------------------------------------------------------------
+// Unit Tests
+// ------------------------------------------------------------------------------------------------
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use serde_json::ser::to_string_pretty;
+
+    // --------------------------------------------------------------------------------------------
+    #[test]
+    fn test_currency_loading() {
+        match lookup_by_alpha(&"GBP".to_string()) {
+            None => println!("lookup_by_alpha NO 'GBP'"),
+            Some(c) => println!("lookup_by_alpha {:#?}", to_string_pretty(c)),
+        }
+    }
 }

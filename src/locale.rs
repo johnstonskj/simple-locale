06d3cf9a71f86cb1a731e9c0c869ef63cfc44446
Simon Johnston <johnstonskj@gmail.com>
2019-08-05 21:36:45 -0700

    Initial documentation complete

--- src/locale.rs
+++ src/locale.rs
@@ -1,16 +1,62 @@
 /*!
 Provides a layer above the `LocaleString` for different locale specifiers.
 
-# The POSIX Locale
+The `Locale` enum represents three forms of locale specification supported
+by the POSIX C API. These are:
 
-The "POSIX", or "C", locale is the minimal locale. It is a rather neutral
-locale which has the same settings across all systems and compilers, and
-therefore the exact results of a program using this locale are predictable.
-This is the locale used by default on all C programs.
+1. The identifier "POSIX", or "C" is known as the _minimal locale_. It is
+   a rather neutral locale which has the same settings across all systems and
+   compilers, and therefore the exact results of a program using this locale
+   are predictable. This is the locale used by default on all C programs.
+2. A path, starting with the '/' character and which resolves to a directory
+   containing the POSIX definition of a locale.
+3. A locale string, represented in this crate as a `LocaleString` structure
+   that effectively represents a language with cultural qualification.
 
-# Example
+The `Locale::from_str` method can be used to parse any of these kinds into
+the separate enumeration values.
 
-TBD
+## Examples
+
+```
+use simple_locale::locale::Locale;
+use std::str::FromStr;
+
+let locale = Locale::from_str("C");
+assert_eq!(locale.to_string(), "POSIX");
+
+let locale = Locale::from_str("en_US.UTF-8@Latn");
+if let Locate::String(locate_str) = locale {
+    assert_eq!(locale_str.get_language(), "en".to_string());
+    assert_eq!(locale_str.get_territory(), Ok("US".to_string()));
+    assert_eq!(locale_str.get_code_set(), Ok("en".to_string()));
+    assert_eq!(locale_str.get_modifer(), Ok("en".to_string()));
+}
+```
+
+The following example is a more complete use case, the need to parse
+the commonly used `LC_ALL` environment variable to determine it's type
+and potential components.
+
+```
+use simple_locale::locale::Locale;
+use std::str::FromStr;
+use std::env;
+
+if let Ok(lc_str) = env::var("LC_ALL") {
+    match Locale::from_str(&lc_str) {
+        Some(Locale::POSIX) =>
+            println!("POSIX minimal locale"),
+        Some(Locale::Path(p)) =>
+            println!("Path locale"),
+        Some(Locale::String(s)) =>
+            println!("String locale"),
+        _ => "Parse Error",
+    }
+}
+
+```
+
 */
 
 use std::fmt;
@@ -24,9 +70,15 @@ use crate::string::{LocaleString, ParseError};
 // Public Types
 // ------------------------------------------------------------------------------------------------
 
+/// This enumeration represents the three types of Locale specifiers
+/// commonly used by operating systems.
+#[derive(Debug)]
 pub enum Locale {
+    /// The minimal locale specified by POSIX.
     POSIX,
+    /// A path to a locale specification.
     Path(PathBuf),
+    /// A locale string, parsed into a structred form.
     String(LocaleString),
 }
 
@@ -71,6 +123,72 @@ impl FromStr for Locale {
                     Ok(Locale::String(LocaleString::from_str(s)?))
                 }
             },
+        }
+    }
+}
+
+// ------------------------------------------------------------------------------------------------
+// Unit Tests
+// ------------------------------------------------------------------------------------------------
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+
+    use super::*;
+
+    // --------------------------------------------------------------------------------------------
+    # [test]
+    fn test_posix_to_string() {
+        assert_eq!(Locale::POSIX.to_string(), "POSIX");
+    }
+
+    # [test]
+    fn test_path_to_string() {
+        let _path = PathBuf::from_str("/usr/share/locale/en_US");
+//        assert_eq!(path.to_string(), "/usr/share/locale/en_US");
+    }
+
+    # [test]
+    fn test_string_to_string() {
+        let locale = LocaleString::new("en".to_string())
+            .with_territory("US".to_string())
+            .with_code_set("UTF-8".to_string());
+        assert_eq!(locale.to_string(), "en_US.UTF-8");
+    }
+
+    // --------------------------------------------------------------------------------------------
+    # [test]
+    fn test_posix_from_string() {
+        match Locale::from_str("POSIX") {
+            Ok(Locale::POSIX) => (),
+            _ => panic!("expecting Locale::POSIX")
+        }
+        match Locale::from_str("C") {
+            Ok(Locale::POSIX) => (),
+            _ => panic!("expecting Locale::POSIX (C)")
+        }
+    }
+
+    # [test]
+    fn test_path_from_string() {
+        match Locale::from_str("/usr/share/locale/en_US") {
+            Ok(Locale::Path(p)) =>
+                assert_eq!(p.to_str(), Some("/usr/share/locale/en_US")),
+            _ => panic!("expecting Locale::Path")
+        }
+    }
+
+    # [test]
+    fn test_string_from_string() {
+        println!("{:#?}", Locale::from_str("en_US.UTF-8"));
+        match Locale::from_str("en_US.UTF-8") {
+            Ok(Locale::String(ls)) => {
+                assert_eq!(ls.get_language_code(), "en");
+                assert_eq!(ls.get_territory(), Some("US".to_string()));
+                assert_eq!(ls.get_code_set(), Some("UTF-8".to_string()));
+            },
+            _ => panic!("expecting Locale::String")
         }
     }
 }
